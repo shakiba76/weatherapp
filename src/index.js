07431c7f3fd5121f378d397e3dd9c7d7799a9c9a
shakiba76 <76821146+shakiba76@users.noreply.github.com>
2023-01-08 23:33:00 +0330

    forecasting-one-problem

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 function search(city) {
-  let apiKey = "8ca7dd4e61360b90fb66918853670e48";
+  let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(`${apiUrl}`).then(showTemperature);
 }
 
 function getForecast(coordinates) {
-  let apiKey = "8ca7dd4e61360b90fb66918853670e48";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(`${apiUrl}`).then(displayForecast);
 }
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-  return days[day];
+  return days[days];
 }
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
@@ -28,6 +28,14 @@ function displayForecast(response) {
         <img src="http://openweathermap.org/img/wn/${
           forecastDay.weather[0].icon
         }@2x.png" alt="" width="42"/>
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
       </div>
       `;
     }

8d7f40973b17bff79bb6cbb675108b028fdde4f7
shakiba76 <76821146+shakiba76@users.noreply.github.com>
2023-01-08 22:27:36 +0330

    inject -js-loop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 function search(city) {
-  let apiKey = "839f4fb8fd4ed856783401ab07d70036";
+  let apiKey = "8ca7dd4e61360b90fb66918853670e48";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(`${apiUrl}`).then(showTemperature);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "8ca7dd4e61360b90fb66918853670e48";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then(displayForecast);
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
+}
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.daily;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      let forecastHtml = `<div class="row">`;
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" width="42"/>
+      </div>
+      `;
+    }
+  });
+  forecastHtml = forecastHtml + `</div>`;
+  forecastElement.innerHTML = forecastHtml;
 }
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#city-temperature");
-  temperatureElement.innerHTML = `${temperature}`;
+  temperatureElement.innerHTML = `${temperature}°c`;
   let descriptionWeather = document.querySelector("#description");
   descriptionWeather.innerHTML = `${response.data.weather[0].description}`;
   let date = document.querySelector(".name-day");
@@ -16,6 +48,7 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 function submittingCity(event) {
   event.preventDefault();
@@ -31,14 +64,15 @@ function formatDate(timestamp) {
   let minutes = date.getMinutes();
   return `${day} ${hour}:${minutes}`;
 }
+
 let days = [
-  "Saturday",
   "Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
   "Thursday",
   "Friday",
+  "Saturday",
 ];
 search("Teran");
 let form = document.querySelector(".searching-city");
